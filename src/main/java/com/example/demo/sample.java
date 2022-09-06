7c8d0116466c1c7601c3b1b3a0386a95e5b3327f
shivam <shivam.ganguly01@gmail.com>
2022-09-06 12:19:46 +0530

    added

--- src/main/java/com/example/demo/sample.java
+++ src/main/java/com/example/demo/sample.java
@@ -1,20 +1,13 @@
 package com.example.demo;
 
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 public class sample {
 	private String name;
 	private int age;
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getAge() {
-		return age;
-	}
-	public void setAge(int age) {
-		this.age = age;
-	}
+	
 
 }
